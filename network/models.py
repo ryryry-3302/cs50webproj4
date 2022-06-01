a5a558198ae94ee39ce11cf1226511d2ea9ac2df
ryryry-3302 <rjleong2002@gmail.com>
2022-06-01 22:01:28 +0800

    updated model

--- network/models.py
+++ network/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 class User(AbstractUser):
+    following = models.ManyToManyField("self", symmetrical=False, related_name="followers" )
     pass
 
 class Post(models.Model):
--- network/static/network/script.js
+++ network/static/network/script.js
@@ -1,10 +1,17 @@
+/// if it doesnt update clear browser cached images :D
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#createbtn').addEventListener('click', function(){
         this.style.display = 'none';
-        document.querySelector('#postform').style.display = 'block'; 
+        document.querySelector('#posts').style.display = 'none';
+        document.querySelector('#postform').style.display = 'block';
+        
+        
     });
     document.querySelector('#backbtn').addEventListener('click', function(){
         document.querySelector('#postform').style.display = 'none';
+        document.querySelector('#posts').style.display = 'block';
         document.querySelector('#createbtn').style.display = 'block';
+        
+        
     });
 });
